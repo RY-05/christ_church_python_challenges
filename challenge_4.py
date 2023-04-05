7249ddbc6da6647469c1f858e202757166ad486c
RY <126905892+RY-05@users.noreply.github.com>
2023-04-05 12:01:00 +0100

    Update challenge_4.py

--- challenge_4.py
+++ challenge_4.py
@@ -1,1 +1,6 @@
 # How long would it take to walk every street in Oxford? 
+
+# Targets:
+# Call the API to find the names of all cities in the UK.
+# Plot the outline of Oxford, and count the number of ‘university’ tagged buildings.
+# Plot the Oxford road network by road type.
