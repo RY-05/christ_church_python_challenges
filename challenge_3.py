540c4d5904e57028cc89dfee9abab7bf0d4dcd83
RY <126905892+RY-05@users.noreply.github.com>
2023-04-05 11:59:52 +0100

    Update challenge_3.py

--- challenge_3.py
+++ challenge_3.py
@@ -1,1 +1,16 @@
 # How fast can a computer read all of Wikipedia?
+
+# Targets:
+# Search and select the ‘Hello World’ page, then print the ‘Summary’ to the terminal.
+
+# Run frequency analysis (ie count the occurrences of each letter) on the content of the ‘Oxford’ page.
+
+# Create a Wordcloud for the ‘Content’ data from a page of your choice.
+
+
+# Extensions:
+# Starting at a page of your choice, choose a random link on the page, and print the title of the page selected.
+
+# Repeat the above random link selection 20 times, printing the title of each site you visit.
+
+# Estimate how long it would take to read all of Wikipedia by calling pages from this API.
