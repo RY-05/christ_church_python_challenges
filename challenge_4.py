bf7f6028ee828ee89a15dfd8fc0851b8843f6657
RY <126905892+RY-05@users.noreply.github.com>
2023-04-05 12:02:13 +0100

    Update challenge_4.py

--- challenge_4.py
+++ challenge_4.py
@@ -2,5 +2,14 @@
 
 # Targets:
 # Call the API to find the names of all cities in the UK.
+
 # Plot the outline of Oxford, and count the number of ‘university’ tagged buildings.
+
 # Plot the Oxford road network by road type.
+
+
+# Extensions:
+# Calculate the length of each of the types of road within Oxford, 
+# and therefore estimate the time it would take to walk all roads.
+
+# Populate your map with the locations of Oxford colleges at the university.
