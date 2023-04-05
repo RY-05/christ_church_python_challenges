70f2b16c9b158a10333025eb45e520ae31e4a0a5
RY <126905892+RY-05@users.noreply.github.com>
2023-04-05 11:06:45 +0100

    Update challenge_1.py

--- challenge_1.py
+++ challenge_1.py
@@ -1,1 +1,11 @@
 # Can computers be used to create art?
+
+# Targets:
+# Plot a circle, using the parametric equation above.
+# Find the parametric equation for Fermat’s Spiral (online), and therefore plot the spiral.
+# Do the same for the Butterﬂy Curve.
+
+# Extensions:
+# Specify and plot 5 circles with diﬀerent sizes and diﬀerent colours.
+# For the Butterﬂy Curve, change the script so that the colour of each line segment changes with the x-coordinate.
+# For Fermat’s spiral, plot the curve so that the colour gets lighter with the radius of each line segment.
