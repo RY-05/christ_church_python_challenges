ff426aa8fe6b135857553d29ceff96f0882c539d
RY <126905892+RY-05@users.noreply.github.com>
2023-04-05 11:56:24 +0100

    Update challenge_2.py

--- challenge_2.py
+++ challenge_2.py
@@ -1,1 +1,19 @@
 # How do computers recognise songs? 
+
+# Targets:
+# Load a song, and then have Python play the song back to you.
+
+# Plot the waveform of the song.
+
+# Plot the logarithm of the spectrogram of your song, against time and frequency.
+
+
+# Extensions:
+# When you calculate and then plot the frequency data, experiment by increasing the time resolution. 
+# What do you notice about the `sharpness' of the frequency if you reduce the time step?
+
+# What you observe is related mathematically to a well-known physics phenomenon. 
+# Can you identify which phenomenon it is related to?
+
+# A paper detailing a popular song-search algorithm is freely available online. 
+# Can you find this paper? How does it use a spectrogram to find songs?
