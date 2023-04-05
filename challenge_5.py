ad98b5627fa23a4bc2d34bafb903fcf49dce8bbf
RY <126905892+RY-05@users.noreply.github.com>
2023-04-05 12:04:14 +0100

    Update challenge_5.py

--- challenge_5.py
+++ challenge_5.py
@@ -1,1 +1,16 @@
 # How do computers modify images?
+
+# Targets:
+# Load an image and then display the image
+
+# Plot the negative of your image.
+
+# Plot a histogram of the RGB values within your image.
+
+
+# Extensions:
+# Choose a small feature in your image and remove it using in-painting.
+
+# Run edge detection on your image using the Canny Edge detector
+
+# Smooth the image using a Gaussian filter.
